ca04e7f104f3f61ff6e2cecacc77570895dccdc6
Gabriel Nunes <gaseinesdzn@hotmail.com>
2023-05-06 12:29:56 -0300

    Update javascript.js

--- aula13Repetições/javascript.js
+++ aula13Repetições/javascript.js
@@ -2,11 +2,11 @@ var c = 1
 var p = 1
 while(c <=5){
     console.log(`${c}`)
-    c++
+    c += 1
 }
 do{
     console.log(`Comer Pizza ${p}`)
-    p++
+    p = p + 1
 }while(p <=5)
 
 for (var t = 1; t <= 5; t++){
