c413ec4d04dea0f555a1f7494ae544c7d8dcb8ac
Gabriel Nunes <gaseinesdzn@hotmail.com>
2023-05-28 19:51:21 -0300

    aulas

---
+++ script.js
@@ -0,0 +1,15 @@
+const code = document.querySelectorAll('code')
+const ex = document.querySelectorAll('p.ex')
+const body = document.querySelector('body')
+
+
+function modoEscuro(){
+    body.style.background = 'black'
+    body.style.color = 'white'
+    
+}
+function modoClaro(){
+    body.style.background = 'white'
+    body.style.color = 'black'
+    
+}
