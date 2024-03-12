90c2013ba77ca31306310bd943ce9d85efdff572
Gaseines <gaseinesdzn@hotmail.com>
2024-03-12 00:17:54 -0300

    + sobre classes

---
+++ uDemy/orientacao a objetos/classes/maisConstructorNaClass/script.js
@@ -0,0 +1,16 @@
+class Cachorro {
+    constructor(raca, cor){
+        this.raca = raca
+        this.cor = cor
+    }
+
+    latir(){
+        console.log("Au Au")
+    }
+}
+
+Cachorro.prototype.patas = 4
+
+let labrador = new Cachorro('Labrador', 'Cinza')
+
+console.log(labrador)
