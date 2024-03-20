e7ddf6a15371c494984f1d028a699440f02447a0
Gaseines <gaseinesdzn@hotmail.com>
2024-03-19 22:47:09 -0300

    bugs e debugs

--- uDemy/orientacao a objetos/exercícios/conta_pupanca/script.js
+++
@@ -1,7 +0,0 @@
-class Banco {
-    constructor(saldoCC, saldoPP, juros){
-        this.saldoCC = saldoCC
-        this.saldoPP = saldoPP
-        this.juros = juros
-    }
-}
--- uDemy/scriptEx.js
+++ uDemy/scriptEx.js
@@ -1,3 +1,5 @@
+
+
 let calculadora = {
     soma: function(num1, num2){
         return num1 + num2
