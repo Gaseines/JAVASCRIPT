a3a6d56d2aee9d608ba05eed62b6ee1f2a44f251
Gaseines <gaseinesdzn@hotmail.com>
2024-04-16 18:44:04 -0300

    começo de programação assíncrona

--- script.js
+++ script.js
@@ -59,6 +59,7 @@ let jsonDiv = document.querySelector('.json')
 let orientacaoObjDiv = document.querySelector('.orientacaoObj')
 let debugDiv = document.querySelector('.debug')
 let expregDiv = document.querySelector('.expreg')
+let assincornaDiv = document.querySelector('.ass')
 
 
 
@@ -76,7 +77,8 @@ let objCheck = document.querySelector('#obj')
 let jsonCheck = document.querySelector('#json')
 let oriObjCheck = document.querySelector('#oriObj')
 let debugCheck = document.querySelector('#debug')
-let rxpregCheck = document.querySelector('#rxpreg')
+let rxpregCheck = document.querySelector('#expreg')
+let assCheck = document.querySelector('#ass')
 
 function clickCheck(dado){
     //1
@@ -97,6 +99,15 @@ function clickCheck(dado){
     }
 
     //3
+    if(dado == str && strCheck.checked == false){
+        stringDiv.style.display = 'none'
+        console.log('certo')
+    }else if(dado == str && strCheck.checked == true){
+        stringDiv.style.display = 'block'
+        console.log('errado')
+    }
+
+    //4
     if(dado == oper && operCheck.checked == false){
         operadoresDiv.style.display = 'none'
         console.log('certo')
@@ -105,111 +116,102 @@ function clickCheck(dado){
         console.log('errado')
     }
 
-    //4
-    if(dado == num && numCheck.checked == false){
-        numerosDiv.style.display = 'none'
-        console.log('certo')
-    }else if(dado == num && numCheck.checked == true){
-        numerosDiv.style.display = 'block'
-        console.log('errado')
-    }
-
     //5
-    if(dado == num && numCheck.checked == false){
-        numerosDiv.style.display = 'none'
+    if(dado == dom && domCheck.checked == false){
+        domDiv.style.display = 'none'
         console.log('certo')
-    }else if(dado == num && numCheck.checked == true){
-        numerosDiv.style.display = 'block'
+    }else if(dado == dom && domCheck.checked == true){
+        domDiv.style.display = 'block'
         console.log('errado')
     }
 
     //6
-    if(dado == num && numCheck.checked == false){
-        numerosDiv.style.display = 'none'
+    if(dado == cond && condCheck.checked == false){
+        condicoesDiv.style.display = 'none'
         console.log('certo')
-    }else if(dado == num && numCheck.checked == true){
-        numerosDiv.style.display = 'block'
+    }else if(dado == cond && condCheck.checked == true){
+        condicoesDiv.style.display = 'block'
         console.log('errado')
     }
 
     //7
-    if(dado == num && numCheck.checked == false){
-        numerosDiv.style.display = 'none'
+    if(dado == rep && repCheck.checked == false){
+        repeticoesDiv.style.display = 'none'
         console.log('certo')
-    }else if(dado == num && numCheck.checked == true){
-        numerosDiv.style.display = 'block'
+    }else if(dado == rep && repCheck.checked == true){
+        repeticoesDiv.style.display = 'block'
         console.log('errado')
     }
 
     //8
-    if(dado == num && numCheck.checked == false){
-        numerosDiv.style.display = 'none'
+    if(dado == arrow && arrowCheck.checked == false){
+        arrowDiv.style.display = 'none'
         console.log('certo')
-    }else if(dado == num && numCheck.checked == true){
-        numerosDiv.style.display = 'block'
+    }else if(dado == arrow && arrowCheck.checked == true){
+        arrowDiv.style.display = 'block'
         console.log('errado')
     }
 
     //9
-    if(dado == num && numCheck.checked == false){
-        numerosDiv.style.display = 'none'
+    if(dado == math && mathCheck.checked == false){
+        mathDiv.style.display = 'none'
         console.log('certo')
-    }else if(dado == num && numCheck.checked == true){
-        numerosDiv.style.display = 'block'
+    }else if(dado == math && mathCheck.checked == true){
+        mathDiv.style.display = 'block'
         console.log('errado')
     }
 
     //10
-    if(dado == num && numCheck.checked == false){
-        numerosDiv.style.display = 'none'
+    if(dado == arr && arrCheck.checked == false){
+        arrayDiv.style.display = 'none'
         console.log('certo')
-    }else if(dado == num && numCheck.checked == true){
-        numerosDiv.style.display = 'block'
+    }else if(dado == arr && arrCheck.checked == true){
+        arrayDiv.style.display = 'block'
         console.log('errado')
     }
 
     //11
-    if(dado == num && numCheck.checked == false){
-        numerosDiv.style.display = 'none'
+    if(dado == obj && objCheck.checked == false){
+        objetosDiv.style.display = 'none'
         console.log('certo')
-    }else if(dado == num && numCheck.checked == true){
-        numerosDiv.style.display = 'block'
+    }else if(dado == obj && objCheck.checked == true){
+        objetosDiv.style.display = 'block'
         console.log('errado')
     }
 
     //12
-    if(dado == num && numCheck.checked == false){
-        numerosDiv.style.display = 'none'
+    if(dado == json && jsonCheck.checked == false){
+        jsonDiv.style.display = 'none'
         console.log('certo')
-    }else if(dado == num && numCheck.checked == true){
-        numerosDiv.style.display = 'block'
+    }else if(dado == json && jsonCheck.checked == true){
+        jsonDiv.style.display = 'block'
         console.log('errado')
     }
 
     //13
-    if(dado == num && numCheck.checked == false){
-        numerosDiv.style.display = 'none'
+    if(dado == oriObj && oriObjCheck.checked == false){
+        orientacaoObjDiv.style.display = 'none'
         console.log('certo')
-    }else if(dado == num && numCheck.checked == true){
-        numerosDiv.style.display = 'block'
+    }else if(dado == oriObj && oriObjCheck.checked == true){
+        orientacaoObjDiv.style.display = 'block'
         console.log('errado')
     }
 
     //14
-    if(dado == num && numCheck.checked == false){
-        numerosDiv.style.display = 'none'
+    if(dado == debug && debugCheck.checked == false){
+        debugDiv.style.display = 'none'
         console.log('certo')
-    }else if(dado == num && numCheck.checked == true){
-        numerosDiv.style.display = 'block'
+    }else if(dado == debug && debugCheck.checked == true){
+        debugDiv.style.display = 'block'
         console.log('errado')
     }
 
     //15
-    if(dado == num && numCheck.checked == false){
-        numerosDiv.style.display = 'none'
+    if(dado == expreg && rxpregCheck.checked == false){
+        expregDiv.style.display = 'none'
         console.log('certo')
-    }else if(dado == num && numCheck.checked == true){
-        numerosDiv.style.display = 'block'
+    }else if(dado == expreg && rxpregCheck.checked == true){
+        expregDiv.style.display = 'block'
         console.log('errado')
     }
 
